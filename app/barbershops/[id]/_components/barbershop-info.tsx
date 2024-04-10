dadda602a3986b590353e502a783e15e76cb7268
Rafael de Lima <rafalimabcl@gmail.com>
2024-04-09 21:20:41 -0300

    fix: fixed indentation

--- app/barbershops/[id]/_components/barbershop-info.tsx
+++ app/barbershops/[id]/_components/barbershop-info.tsx
@@ -45,19 +45,21 @@ const BarbershoInfo = ({ barbershop }: BarberShopDetailsInfoProps) => {
                 objectFit: "cover",
             }}
             className="opacity-75" />
-    </div><div className="px-5 pt-3 pb-6 border-b border-solid border-secondary">
-            <h1 className="text-xl font-bold">{barbershop.name}</h1>
-            <div className="flex items-center gap-1 mt-2">
-                <MapPinIcon className="text-primary" size={18} />
-                <p className="text-sm">{barbershop.address}</p>
-            </div>
+    </div>
+    
+    <div className="px-5 pt-3 pb-6 border-b border-solid border-secondary">
+        <h1 className="text-xl font-bold">{barbershop.name}</h1>
+        <div className="flex items-center gap-1 mt-2">
+            <MapPinIcon className="text-primary" size={18} />
+            <p className="text-sm">{barbershop.address}</p>
+        </div>
 
             <div className="flex items-center gap-1 mt-2">
                 <StarIcon className="text-primary" size={18} />
                 <p className="text-sm">5,0 (789 avaliações)</p>
             </div>
         </div>
-        </div>);
+    </div>);
 }
  
 export default BarbershoInfo;
