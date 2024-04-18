f9aa3e5027d79c1324f093b3b51f5bed634396b5
Rafael de Lima <rafalimabcl@gmail.com>
2024-04-17 21:46:54 -0300

    fix: fixed indentation

--- app/_providers/auth.tsx
+++ app/_providers/auth.tsx
@@ -1,12 +1,10 @@
 "use client";
 
-import { SessionProvider } from "next-auth/react"
+import { SessionProvider } from "next-auth/react";
 import { ReactNode } from "react";
 
 const AuthProvider = ({children}:{children: ReactNode}) => {
-    return ( 
-    <SessionProvider>{children}</SessionProvider>
-    );
-}
+    return <SessionProvider>{children}</SessionProvider>;
+};
  
 export default AuthProvider;
