1b66381e3a2bc993ad38416b1e2adbd6e5d0a9ea
Rafael de Lima <rafalimabcl@gmail.com>
2024-04-17 21:49:13 -0300

    feat: changing the order imported files

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,16 @@
-import { db } from "@/app/_lib/prisma";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import NextAuth, { AuthOptions } from "next-auth"
-import { Adapter } from "next-auth/adapters";
+import NextAuth, { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { Adapter } from "next-auth/adapters";
+
+import { db } from "@/app/_lib/prisma";
 
 export const authOptions: AuthOptions = {
     adapter: PrismaAdapter(db) as Adapter,
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         }),
     ],
 };
