dc2600b6f824f8b6b9f10aa0a48e9eec0a32b1e7
Rafael de Lima <rafalimabcl@gmail.com>
2024-05-27 23:13:11 -0300

    fix: corrected the home page name

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -35,7 +35,7 @@ export default async function Home() {
       <Header />
 
       <div  className="px-5 pt-5">
-      <h2 className="text-xl font-bold">{session?.user ? `Olá ${session.user.name?.split(" ")[0]}` : "Faça o login para agendar um serviço!"}</h2>
+      <h2 className="text-xl font-bold">{session?.user ? `Olá, ${session.user.name?.split(" ")[0]}` : "Faça o login para agendar um serviço!"}</h2>
       <p className="capitalize text-sm">
         {format(new Date(), "EEEE',' d 'de' MMMM", {
         locale: ptBR,
